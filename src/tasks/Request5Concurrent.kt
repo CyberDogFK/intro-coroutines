93a8b52888c9e7c33a8c78476d152c18172b29ed
Anton Pavliuk <streetreadger@gmail.com>
2023-11-29 22:57:51 +0200

    Implement task 5

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,19 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    async {
+        service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+    }.await().map { repo ->
+        async {
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }.awaitAll()
+        .flatten()
+        .aggregate()
 }
