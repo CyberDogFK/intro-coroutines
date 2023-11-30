56600c5fb121bd4b83125427141ceff248992ad7
Anton Pavliuk <streetreadger@gmail.com>
2023-11-30 13:44:41 +0200

    Implemented task 6

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,26 @@
 package tasks
 
 import contributors.*
+import java.util.concurrent.CountDownLatch
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    var aggregatedList = listOf<User>()
+    repos.forEachIndexed { index, repo ->
+        log("starting loading for ${repo.name}")
+        val res = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        aggregatedList = (aggregatedList + res).aggregate()
+        updateResults(aggregatedList, index == repos.lastIndex)
+    }
 }
